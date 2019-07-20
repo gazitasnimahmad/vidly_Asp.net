1a582a68d508c032d0b6948ae5178c181736cfab
gazitasnimahmad <39596910+gazitasnimahmad@users.noreply.github.com>
2019-07-20 08:12:16 -0700

    Add project files.

---
+++ vidly_Asp.net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vidly_Asp.net.Startup))]
+namespace vidly_Asp.net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
